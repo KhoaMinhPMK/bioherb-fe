a2595e64b9dfc93e1f4cec33bc85be9caf7c85a7
KhoaMinhPMK <pmkkhoaminh@gmail.com>
2026-04-02 18:44:15 +0700

    fix(backend): require compression to avoid ESM interop issue on Node 24

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
-import compression from 'compression';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const compression = require('compression');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
